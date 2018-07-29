118e45fcfb31746ba1ab4ba6d7f7736bee45fa91
Rebecca Park <burugirl93@gmail.com>
2018-07-28 22:21:56 -0400

    Get admin-ui running (#3)
    * Get admin-ui running
    
    * Run lint:fix

--- api/src/index.js
+++ api/src/index.js
@@ -9,18 +9,22 @@ function initializeDatabase() {
   return new Sequelize('sqlite:nikolaus.db');
 }
 
-function main() {
+async function main() {
   const app = express();
   app.db = initializeDatabase();
 
-  migrations.migrateUp(app.db);
-  migrations.seedUp(app.db);
+  await migrations.migrateUp(app.db);
+  await migrations.seedUp(app.db);
 
   models.register(app);
   services.register(app);
 
   // eslint-disable-next-line no-console
-  app.listen(3000, () => console.log('Listening on port 3000'));
+  app.listen(8000, () => console.log('Listening on port 3000'));
 }
 
-main();
+main().catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error(e);
+  process.exit(1);
+});
