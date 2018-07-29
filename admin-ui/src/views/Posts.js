19eef96e505451a535abb08b7b8e45107a7815bb
Rebecca Park <burugirl93@gmail.com>
2018-07-28 22:03:43 -0400

    Get admin-ui running

--- admin-ui/src/views/Posts.js
+++ admin-ui/src/views/Posts.js
@@ -16,19 +16,19 @@ export default function () {
     {
       value: 'comments',
       width: '0.5fr',
-      body: data => <IconColumn icon="icon ion-ios-chatbubbles-outline" data={data} />,
+      format: data => <IconColumn icon="icon ion-ios-chatbubbles-outline" data={data} />,
     },
     {
       value: 'hearts',
       width: '0.5fr',
-      body: data => <IconColumn icon="icon ion-ios-heart-outline" data={data} />,
+      format: data => <IconColumn icon="icon ion-ios-heart-outline" data={data} />,
     },
   ];
 
   const data = [
     {
       id: 1,
-      title: 'Carrot cake with a twist',
+      title: 'Chocolate cake with a twist',
       publishedDate: 'Feb 10, 2017,',
       publishedBy: 'Rebecca Park',
       lastModified: '22 seconds ago',
