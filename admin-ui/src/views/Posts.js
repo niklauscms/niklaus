fa7403c09591815a0408ee2f0aad47c13ff350ec
Rebecca Park <burugirl93@gmail.com>
2018-07-29 00:05:19 -0400

    Fetch posts from API

--- admin-ui/src/views/Posts.js
+++ admin-ui/src/views/Posts.js
@@ -1,60 +1,59 @@
 import React from 'react';
 import Table, { IconColumn } from 'react-css-grid-table';
 
-export default function () {
-  const columns = [
-    {
-      value: 'check',
-      width: '0.3fr',
-      format: data => <IconColumn icon="icon ion-md-checkmark" data={data} />,
-      className: 'justify-content-center',
-    },
-    { label: 'Title', value: 'title', width: '2fr' },
-    { label: 'Published Date', value: 'publishedDate', width: '1fr' },
-    { label: 'Published By', value: 'publishedBy', width: '1fr' },
-    { label: 'Last Modified', value: 'lastModified', width: '1fr' },
-    {
-      value: 'comments',
-      width: '0.5fr',
-      format: data => <IconColumn icon="icon ion-ios-chatbubbles-outline" data={data} />,
-    },
-    {
-      value: 'hearts',
-      width: '0.5fr',
-      format: data => <IconColumn icon="icon ion-ios-heart-outline" data={data} />,
-    },
-  ];
+async function request(endpoint) {
+  const response = await fetch(`http://localhost:8000/${endpoint}`);
+  return response.json();
+}
 
-  const data = [
-    {
-      id: 1,
-      title: 'Chocolate cake with a twist',
-      publishedDate: 'Feb 10, 2017,',
-      publishedBy: 'Rebecca Park',
-      lastModified: '22 seconds ago',
-      comments: 22,
-      hearts: 10,
-    },
-    {
-      id: 2,
-      title: 'Green tea rice cake',
-      publishedDate: 'Nov 27, 2017',
-      publishedBy: 'Mumu Eaton',
-      lastModified: '1 day ago',
-      comments: 13,
-      hearts: 11,
-    },
-  ];
+export default class extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="Posts">
-      <h1>
-Posts
-      </h1>
-      <Table
-        columns={columns}
-        data={data}
-      />
-    </div>
-  );
+    this.state = { posts: [] };
+  }
+
+  async componentDidMount() {
+    const posts = await request('posts');
+    this.setState({ posts });
+  }
+
+  render() {
+    const columns = [
+      {
+        value: 'check',
+        width: '0.3fr',
+        format: data => <IconColumn icon="icon ion-md-checkmark" data={data} />,
+        className: 'justify-content-center',
+      },
+      { label: 'Title', value: 'title', width: '2fr' },
+      { label: 'Published Date', value: 'publishedDate', width: '1fr' },
+      { label: 'Published By', value: 'publishedBy', width: '1fr' },
+      { label: 'Last Modified', value: 'lastModified', width: '1fr' },
+      {
+        value: 'comments',
+        width: '0.5fr',
+        format: data => <IconColumn icon="icon ion-ios-chatbubbles-outline" data={data} />,
+      },
+      {
+        value: 'hearts',
+        width: '0.5fr',
+        format: data => <IconColumn icon="icon ion-ios-heart-outline" data={data} />,
+      },
+    ];
+
+    const { posts: data } = this.state;
+
+    return (
+      <div className="Posts">
+        <h1>
+          Posts
+        </h1>
+        <Table
+          columns={columns}
+          data={data}
+        />
+      </div>
+    );
+  }
 }
